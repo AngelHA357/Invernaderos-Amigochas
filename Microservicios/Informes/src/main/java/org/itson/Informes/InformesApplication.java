e74a3404456c3fde9821e1a90ae853ae879e55f2
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2025-04-30 22:50:52 -0700

    Microservicio de Informes

---
+++ Microservicios/Informes/src/main/java/org/itson/Informes/InformesApplication.java
@@ -0,0 +1,13 @@
+package org.itson.Informes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformesApplication.class, args);
+	}
+
+}
