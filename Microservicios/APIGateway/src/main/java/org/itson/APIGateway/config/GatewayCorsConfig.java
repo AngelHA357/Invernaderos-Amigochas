38e44eedcd3320c1b7ef3f989c33eabcb31e59f0
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2025-05-03 13:57:10 -0700

    resolví el 403 pero ahora sale 503

--- Microservicios/APIGateway/src/main/java/org/itson/APIGateway/config/GatewayCorsConfig.java
+++ Microservicios/APIGateway/src/main/java/org/itson/APIGateway/config/GatewayCorsConfig.java
@@ -25,7 +25,7 @@ public class GatewayCorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
