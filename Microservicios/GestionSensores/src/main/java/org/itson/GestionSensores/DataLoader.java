f5cc8bff187da4d1f5b157045b250e1643aa8997
bortcrab <bortcrab@gmail.com>
2025-05-21 21:44:47 -0700

    Merge branch 'main' into Empresas-Distribuidas

--- Microservicios/GestionSensores/src/main/java/org/itson/GestionSensores/DataLoader.java
+++ Microservicios/GestionSensores/src/main/java/org/itson/GestionSensores/DataLoader.java
@@ -37,31 +37,31 @@ public class DataLoader {
 
                 // Insertar dos sensores por invernadero con datos completos
                 // Invernadero A - Sensores
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-A01", "AA:BB:CC:DD:EE:FF", "SensorTech", "ST-100", "Temperatura", "°C", new ObjectId(invA.get_id()), "Sector 1", "Fila A", true));
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-A02", "AA:BB:CC:DD:EE:01", "SensorTech", "ST-101", "Humedad", "%", new ObjectId(invA.get_id()), "Sector 2", "Fila B", true));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-A01", "AA:BB:CC:DD:EE:FF", "SensorTech", "ST-100", "Temperatura", "°C", new ObjectId(invA.get_id()), "Sector 1", "Fila A"));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-A02", "AA:BB:CC:DD:EE:01", "SensorTech", "ST-101", "Humedad", "%", new ObjectId(invA.get_id()), "Sector 2", "Fila B"));
 
                 // Invernadero B - Sensores
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-B01", "BB:CC:DD:EE:FF:01", "EcoSense", "ES-200", "Temperatura", "°C", new ObjectId(invB.get_id()), "Sector 1", "Fila A", true));
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-B02", "BB:CC:DD:EE:FF:02", "EcoSense", "ES-201", "Humedad", "%", new ObjectId(invB.get_id()), "Sector 2", "Fila B", true));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-B01", "BB:CC:DD:EE:FF:01", "EcoSense", "ES-200", "Temperatura", "°C", new ObjectId(invB.get_id()), "Sector 1", "Fila A"));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-B02", "BB:CC:DD:EE:FF:02", "EcoSense", "ES-201", "Humedad", "%", new ObjectId(invB.get_id()), "Sector 2", "Fila B"));
 
                 // Invernadero C - Sensores
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-C01", "CC:DD:EE:FF:01:02", "TempTech", "T-300", "Temperatura", "°C", new ObjectId(invC.get_id()), "Sector 1", "Fila A", true));
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-C02", "CC:DD:EE:FF:01:03", "TempTech", "T-301", "Humedad", "%", new ObjectId(invC.get_id()), "Sector 2", "Fila B", true));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-C01", "CC:DD:EE:FF:01:02", "TempTech", "T-300", "Temperatura", "°C", new ObjectId(invC.get_id()), "Sector 1", "Fila A"));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-C02", "CC:DD:EE:FF:01:03", "TempTech", "T-301", "Humedad", "%", new ObjectId(invC.get_id()), "Sector 2", "Fila B"));
 
                 // Invernadero D - Sensores
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-D01", "DD:EE:FF:01:02:03", "GreenGrow", "GG-500", "Temperatura", "°C", new ObjectId(invD.get_id()), "Sector 1", "Fila A", true));
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-D02", "DD:EE:FF:01:02:04", "GreenGrow", "GG-501", "Humedad", "%", new ObjectId(invD.get_id()), "Sector 2", "Fila B", true));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-D01", "DD:EE:FF:01:02:03", "GreenGrow", "GG-500", "Temperatura", "°C", new ObjectId(invD.get_id()), "Sector 1", "Fila A"));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-D02", "DD:EE:FF:01:02:04", "GreenGrow", "GG-501", "Humedad", "%", new ObjectId(invD.get_id()), "Sector 2", "Fila B"));
 
                 // Invernadero E - Sensores
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-E01", "EE:FF:01:02:03:04", "AgroSense", "AS-400", "Temperatura", "°C", new ObjectId(invE.get_id()), "Sector 1", "Fila A", true));
-                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-E02", "EE:FF:01:02:03:05", "AgroSense", "AS-401", "Humedad", "%", new ObjectId(invE.get_id()), "Sector 2", "Fila B", true));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-E01", "EE:FF:01:02:03:04", "AgroSense", "AS-400", "Temperatura", "°C", new ObjectId(invE.get_id()), "Sector 1", "Fila A"));
+                gestionSensoresRepository.save(new Sensor(new ObjectId(), "SEN-E02", "EE:FF:01:02:03:05", "AgroSense", "AS-401", "Humedad", "%", new ObjectId(invE.get_id()), "Sector 2", "Fila B"));
 
             // Insertar sensores
-            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000001"), "SEN-0101", "AA:BB:CC:DD:EE:FF", "SensorTech", "ST-100", "Humedad", "%", new ObjectId(invA.get_id()), "Sector 1", "Fila A", true));
-            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000002"),"SEN-0202", "11:22:33:44:55:66", "EcoSense", "ES-200", "Temperatura", "C", new ObjectId(invB.get_id()), "Sector 2", "Fila B", true));
-            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000003"),"SEN-0303", "AA:11:BB:22:CC:33", "TempTech", "T-300", "Temperatura", "F", new ObjectId(invC.get_id()), "Sector 1", "Fila A", true));
-            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000004"),"SEN-0404", "66:55:44:33:22:11", "GreenGrow", "GG-500", "Temperatura", "K", new ObjectId(invD.get_id()), "Sector 1", "Fila A", true));
-            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000005"),"SEN-0505", "77:88:99:AA:BB:CC", "AgroSense", "AS-400", "Humedad", "%", new ObjectId(invE.get_id()), "Sector 2", "Fila B", true));
+            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000001"), "SEN-0101", "AA:BB:CC:DD:EE:FF", "SensorTech", "ST-100", "Humedad", "%", new ObjectId(invA.get_id()), "Sector 1", "Fila A"));
+            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000002"),"SEN-0202", "11:22:33:44:55:66", "EcoSense", "ES-200", "Temperatura", "C", new ObjectId(invB.get_id()), "Sector 2", "Fila B"));
+            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000003"),"SEN-0303", "AA:11:BB:22:CC:33", "TempTech", "T-300", "Temperatura", "F", new ObjectId(invC.get_id()), "Sector 1", "Fila A"));
+            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000004"),"SEN-0404", "66:55:44:33:22:11", "GreenGrow", "GG-500", "Temperatura", "K", new ObjectId(invD.get_id()), "Sector 1", "Fila A"));
+            gestionSensoresRepository.save(new Sensor(new ObjectId("661eff4b0000000000000005"),"SEN-0505", "77:88:99:AA:BB:CC", "AgroSense", "AS-400", "Humedad", "%", new ObjectId(invE.get_id()), "Sector 2", "Fila B"));
             System.out.println("Se han insertado 5 sensores");
         };
     }
